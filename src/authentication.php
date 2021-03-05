ddd803ae619c7c310b278efbe87937a0cb769046
csonipi <csoni.pi@gmail.com>
2021-03-05 18:45:14 +0100

    Edited getToken method

--- src/authentication.php
+++ src/authentication.php
@@ -1,21 +1,27 @@
 <?php
-
-function prepare_headers( string $token ): array {
-	return array(
-		'Content-Type'  => 'application/json',
-		'Accept'        => 'application/json',
-		'Authorization' => 'Bearer ' . $token
-	);
-}
-
 function getToken(){
 	$url = "http://api.ilogistic.eu/authentication/login";
-	$fields = [
-	    'header '  => prepare_headers(''),
-	    'Email'    => "",
-	    'Password' => ""
-	];
-	cUrlPost($url, $fields);
+	$fields = "Email=&Password=";
+	
+	$ch = curl_init();
+	
+	$headers = array();
+	$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+		
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+	
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields);
+	
+	$obj = json_decode(curl_exec($ch), true);
+	
+	//Visualize
+	var_dump($obj);
 }
 
 function auth(string $token){
